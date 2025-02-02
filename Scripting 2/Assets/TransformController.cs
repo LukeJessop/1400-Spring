27b4f00e8c2f0724943e8a2ef42730687634b2ef
LukeJessop <jessop.lucas@gmail.com>
2025-02-02 14:50:26 -0700

    updated script for challenge

--- Scripting 2/Assets/TransformController.cs
+++ Scripting 2/Assets/TransformController.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 
 public class TransformController : MonoBehaviour
 {
+    public float speed = 7f;
+    public float distance = 3f;
     private void Update()
     {
-        var x = Mathf.PingPong(Time.time, 3);
-        var p = new Vector3(0, x, 0);
+        var x = Mathf.PingPong(Time.time * speed, distance);
+        var y = Mathf.PingPong(Time.time * speed, distance);
+        var z = Mathf.PingPong(Time.time * speed, distance);
+        var p = new Vector3(x, 0, 0);
         transform.position = p;
         
-        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
+        transform.Rotate(new Vector3(45, 0, 0) * Time.deltaTime);
     }
 }
